d582923d0b91731ef0531c2e22f22ad9dbec9d0a
禹賢祐 <whw3312@gmail.com>
2021-06-13 15:19:46 +0900

    no message

--- test/src/js/cerrent/index.js
+++ test/src/js/cerrent/index.js
@@ -131,28 +131,37 @@ var RenderCrrent = /** @class */ (function () {
             }
             else {
                 if (this.disabled) {
-                    if (this.getCurrentDate().year >= this.yearElm.value) {
-                        if (this.getCurrentDate().month >= this.monthElm.value) {
-                            if (this.getCurrentDate().month > this.monthElm.value && i > this.getCurrentDate().day) {
-                                arrDates.push("<div data-setdate class=\"calendarDay\">" + date[i] + "</div>");
-                            }
-                            else {
-                                arrDates.push("<div data-setdate class=\"calendarDay is-disabled\">" + date[i] + "</div>");
-                            }
+                    // 지금 년수 보다 선택된 년수가 크거나 같을경우
+                    // 선택된년수가 현재 년수 보다 작으면 선택 불가
+                    if (year < this.getCurrentDate().year) {
+                        arrDates.push("<div data-setdate class=\"calendarDay is-disabled\">" + date[i] + "</div>");
+                    }
+                    else {
+                        // 선택된년수가 현재 년수 보다 작으면 선택 불가
+                        if (month < this.getCurrentDate().month) {
+                            arrDates.push("<div data-setdate class=\"calendarDay is-disabled\">" + date[i] + "</div>");
                         }
                         else {
                             arrDates.push("<div data-setdate class=\"calendarDay\">" + date[i] + "</div>");
                         }
                     }
-                    else {
-                        arrDates.push("<div data-setdate class=\"calendarDay\">" + date[i] + "</div>");
-                    }
-                    // else {
+                    // if(year <= this.getCurrentDate().year && month < this.getCurrentDate().month) {
+                    //   arrDates.push(`<div data-setdate class="calendarDay is-disabled">${date[i]}</div>`)
+                    //   if(year < this.getCurrentDate().year){
+                    //     arrDates.push(`<div data-setdate class="calendarDay is-disabled">${date[i]}</div>`)
+                    //   }
+                    // } else {
+                    //   arrDates.push(`<div data-setdate class="calendarDay">${date[i]}</div>`)
+                    // }
+                    // if(this.getCurrentDate().year >= this.yearElm.value ) {
                     //   if (this.getCurrentDate().month > this.monthElm.value) {
+                    //     console.log(i)
                     //     arrDates.push(`<div data-setdate class="calendarDay is-disabled">${date[i]}</div>`)
                     //   } else {
-                    //     arrDates.push(`<div data-setdate class="calendarDay workday">${date[i]}</div>`)
+                    //     arrDates.push(`<div data-setdate class="calendarDay">${date[i]}</div>`)
                     //   }
+                    // } else {
+                    //   arrDates.push(`<div data-setdate class="calendarDay">${date[i]}</div>`)
                     // }
                 }
                 else {
