ad118e22aec3973e9014fbacd18a322f1f2ee69f
禹賢祐 <whw3312@gmail.com>
2021-10-09 22:23:24 +0900

    no message

--- test/src/js/app.js
+++ test/src/js/app.js
@@ -3,10 +3,9 @@ const inputDate = document.querySelector('[data-inputDate]');
 const thunderFuryDatePicker = document.querySelector('.thunderFury-datePicker');
 const thunderDatePicker = new ThunderDatePicker(thunderFuryDatePicker, {
   inputDate,
-  today: new Date().getDate(),
-  weekend: true,
-  // disabled: true
+  // weekend: true,
+  disabled: true
 })
-console.log(thunderDatePicker)
 thunderDatePicker.init();
+
 
